e9e678e4e7d1f100e70da859869c00869e710c49
Z <z>
2023-03-24 17:00:11 -0400

    feat: add favicon

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -3,6 +3,24 @@
  * This is especially useful for Docker builds and Linting.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+];
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -11,6 +29,24 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
+  images: {
+    domains: [        
+      "googleusercontent.com",
+      "unsplash.com",
+      "loremflickr.com",
+      "res.cloudinary.com",
+      "cloudinary.com",
+    ],
+  },
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default config;
