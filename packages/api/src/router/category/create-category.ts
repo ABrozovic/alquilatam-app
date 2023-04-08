2355e5db4396e648d988630a9316a07a0e456f29
Z <z>
2023-04-08 12:34:47 -0400

    feat: update schemas

--- packages/api/src/router/category/create-category.ts
+++ packages/api/src/router/category/create-category.ts
@@ -1,28 +1,21 @@
 import { TRPCError } from "@trpc/server";
-import { type z } from "zod";
+import { z } from "zod";
 
-import { Prisma, type PrismaClient } from "@acme/db";
+import { type PrismaClient } from "@acme/db";
 
-import {
-  autocompleteSchema,
-  categoryImageSchema,
-  categorySchema,
-} from "./schema";
-
-const createAutocompleteSchema = autocompleteSchema.omit({
-  id: true,
-});
-
-const createCategoryImageSchema = categoryImageSchema.omit({ id: true });
+import { categoryImageSchema, categorySchema, fileSchema } from "./schema";
 
 export const createCategorySchema = categorySchema
-  .omit({ id: true, image: true, autocomplete: true })
+  .omit({ id: true, image: true })
   .extend({
-    image: createCategoryImageSchema,
-    autocomplete: createAutocompleteSchema.array().optional(),
+    image: categoryImageSchema.omit({ categoryId: true, id: true }),
   });
-
 export type CreateCategory = z.infer<typeof createCategorySchema>;
+
+export const createCategoryForm = createCategorySchema
+  .omit({ image: true })
+  .extend({ image: fileSchema });
+export type CreateCategoryForm = z.infer<typeof createCategoryForm>;
 
 export const createCategory = async ({
   prisma,
@@ -31,7 +24,7 @@ export const createCategory = async ({
   prisma: PrismaClient;
   data: CreateCategory;
 }) => {
-  const { autocomplete, image, ...category } = data;
+  const { image, ...category } = data;
   const categoryExists = await prisma.category.findFirst({
     where: { name: category.name },
   });
@@ -46,18 +39,10 @@ export const createCategory = async ({
       data: {
         ...category,
         image: { create: { ...image } },
-        ...(autocomplete && {
-          autocomplete: {
-            create: autocomplete.map((autoCompleteItem) => autoCompleteItem),
-          },
-        }),
       },
     });
   } catch (error) {
-    if (
-      error instanceof Prisma.PrismaClientKnownRequestError &&
-      error.code === "P2002"
-    ) {
+    if (isPrismaErrorP2002(error)) {
       throw new TRPCError({
         code: "CONFLICT",
         message: "Category already exists",
@@ -69,3 +54,11 @@ export const createCategory = async ({
     });
   }
 };
+interface CustomPrismaClientKnownRequestError extends Error {
+  code: string;
+}
+export function isPrismaErrorP2002(
+  error: Error | unknown,
+): error is CustomPrismaClientKnownRequestError {
+  return error instanceof Error && "code" in error && error.code === "P2002";
+}
